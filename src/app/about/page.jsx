7d8e0844e1d1b17516d95dfb166ec80526877bda
Juan Cardona <juanscr24@hotmail.com>
2023-06-28 11:54:44 -0500

    new page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import styles from './page.module.css'
+import Image from 'next/image'
+import Button from '@/components/Button/Button'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image 
+          src="/7.jpg" 
+          fill={true}  
+          alt='imagen japonesa' 
+          className={styles.img} 
+        />
+        <div className={styles.imgText}>
+          <h1 className={styles.imgTitle}>Digital Storytellers</h1>
+          <h2 className={styles.imgDesc}>Hadcrafting award winning digital experiences</h2>
+        </div>
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who Are  We?</h1>
+          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae similique quaerat unde est numquam eveniet itaque voluptate, quae consectetur illo, dolorum ad dolorem eum ullam corrupti repellendus possimus? Fuga, ipsum!
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere deleniti consequatur cumque quasi ipsam doloribus alias similique cum nam corporis accusantium culpa, iure sunt fugit incidunt, dolorem optio, tempora reiciendis.</p>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>What We Do?</h1>
+          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit provident officiis quidem culpa qui exercitationem, laboriosam esse aperiam eum aliquam ipsa. Aut itaque distinctio sequi quas, voluptatem nam vero iure?
+          <br />
+          <br />
+          -Creative Ilustration
+          <br />
+          <br />
+          -Dinamic Websites
+          <br />
+          <br />
+          -Mobile Apps Development
+          </p>
+          <Button url="/contact" text="Contact" />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,18 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Button from '@/components/Button/Button'
 
 export default function Home() {
   return (
-    <div>
-      <div className={styles.texts}>
-        <h1>Better design for your digital products</h1>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products</h1>
+        <p className={styles.desc}>Turning Your Idea Into Reality. We Bring together the teams from the global tech industry</p>
+        <Button url="/portafolio" text="See Our Works" />
       </div>
-      <div className={styles.imgContainer}></div>
-      <Image src="/8.jpg" width={500} height={500} alt="foto-general"/>
+      <div className={styles.item}>
+        <Image className={styles.img} src="/8.jpg" width={500} height={500} alt="foto-general"/>
+      </div>
     </div>
   )
 }
